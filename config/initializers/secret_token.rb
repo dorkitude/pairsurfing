878a0864b3ea4bec6902f5d9cee8f353f3772276
dorkitude <kyle@kylewild.com>
2011-06-29 23:27:06 -0500

    initial commit, dogs!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pairsurfing::Application.config.secret_token = '0f1ec39699ca9ad4f85f5281927a89907c4db771b0242c2db44a2fb117033c3b0e28748ade8011b7d1b4a7b387657b0d4baa4aa3e4d5d3af7412e5a9deca07a9'
